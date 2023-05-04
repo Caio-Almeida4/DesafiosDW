7f9b2d23c1b7371fec8593a38ddcd492d2a1fbf4
Caio <caio2003@outlook.pt>
2023-05-04 11:51:34 -0300

    organizacao do rep + conexao com o BD e formulario funcional

---
+++ src/app.py
@@ -0,0 +1,35 @@
+from flask import Flask, render_template, request, url_for
+from flask_mysqldb import MySQL
+
+app = Flask("__name__")
+
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = '1212'
+app.config['MYSQL_DB'] = 'desafiodw'
+
+mysql = MySQL(app)
+
+@app.route("/") # criando rotas com decorator
+def index(): # função para retornar uma mensagem 
+    return render_template("index.html")
+
+@app.route("/contato", methods=['GET','POST']) 
+def contato(): 
+
+    if request.method =='POST':
+        email = request.form['email']
+        assunto = request.form['assunto']
+        descricao = request.form['descricao']
+        cur = mysql.connection.cursor()
+        cur.execute(' INSERT INTO contato VALUES(%s,%s,%s)',(email,assunto,descricao))
+        mysql.connection.commit()
+        cur.close()
+        return 'Oba'
+
+    return render_template("contato.html")
+
+
+@app.route("/quem_somos") 
+def quem_somos(): 
+    return render_template("quem_somos.html")
